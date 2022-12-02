03f9f4ef5b8a9093d93aa652d7b23ce197b1dbfb
codma <ruddud1123@naver.com>
2022-12-03 03:33:52 +0900

    readme 기존 내용 삭제

--- src/mixins/tools.ts
+++ src/mixins/tools.ts
@@ -2,7 +2,7 @@ export const priceFormatter = new Intl.NumberFormat('ko-KR', {
   style: 'currency', 
   currency: 'KRW',
   maximumFractionDigits: 2,
-  minimumFractionDigits: 2,
+  minimumFractionDigits: 0,
 })
 
 
