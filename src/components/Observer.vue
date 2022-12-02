6613a2a1f51b87405003689bf3f186b7bc07e8b9
codma <ruddud1123@naver.com>
2022-12-02 19:05:42 +0900

    랭크 더보기 무한 스크롤 추가

---
+++ src/components/Observer.vue
@@ -0,0 +1,47 @@
+<template>
+  <div ref="trigger"></div>
+</template>
+
+<script setup lang="ts">
+
+import { onMounted, onUnmounted, ref } from 'vue';
+
+  const trigger = ref()
+  const options = {
+    root: null,
+    threshold: 1
+  }
+
+  let observer: any = null
+  const handleIntersect = (entry: any) => {
+    if (entry.isIntersecting) emit('triggerIntersected', trigger.value)
+  }
+
+  const emit = defineEmits<{ 
+    (e: 'triggerIntersected', payload: any): void
+  }>()
+
+  onMounted(() => {
+    try {
+      observer = new IntersectionObserver(entries => {
+        handleIntersect(entries[0])
+      }, options)
+      observer.observe(trigger.value)
+    } catch (e) {
+      console.log(e)
+    }
+  }) 
+
+  onUnmounted(() => {
+    observer.value?.disconnect();
+  });
+  
+
+  
+
+
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -6,13 +6,14 @@
       class="MarketCard"
       theme="dark"
       color="#333333"
+      elevation="3"
     >
       <v-card-title class="mt-3"> 시장 동향 </v-card-title>
       <v-card-subtitle> 주식 시장이 어떻게 변하고 있는지 알아보세요. </v-card-subtitle>      
       <v-btn       
         class="vBtn"  
         variant="text"
-        @click="router.push('/market')" 
+        @click="$router.push('/market')" 
         icon="mdi-chevron-right"
       />
     </v-card>
@@ -23,26 +24,31 @@
       class="MarketCard"      
       theme="dark"   
       color="#333333"   
+      elevation="3"
     >
       <v-card-title class="mt-3"> 추천 종목 </v-card-title>
       <v-card-subtitle class="vCardSubtitle"> DONDA 가 기업 가치가 상대적으로 저평가된 종목들을 추천해줍니다.</v-card-subtitle>
       <v-btn       
         class="vBtn"  
         variant="text"
-        @click="router.push('/market')" 
+        @click="$router.push('/market')" 
         icon="mdi-chevron-right"
       />
     </v-card>
     
     <div class="d-flex justify-space-between mt-8 align-end">
       <v-card-title class="ml-3" v-font-size="16">
-        시가총액이 높은 기업들의 현황입니다.
+        시가총액이 높은 기업들입니다.
       </v-card-title>      
-      <v-btn variant="plain" v-font-size="12">
+      <v-btn 
+        variant="plain" 
+        v-font-size="12" 
+        class="mr-2"
+        @click="$router.push('/rank')"
+      >
         자세히..
       </v-btn>
     </div>
-
 
     <v-card
       v-for="(rankContent, i) in rankContents"
@@ -52,46 +58,70 @@
       class="MarketCard"      
       theme="dark"
       color="#333333"
+      elevation="2"
     >
       <v-card-subtitle v-font-size="15" class="mt-2"> {{ rankContent.market }} </v-card-subtitle>
       <div class="d-flex align-center">
-        <v-card-title v-font-size="18"> {{ rankContent.title }} </v-card-title>
-        
-      </div>
-      
-      
-    </v-card>    
+        <v-card-title v-font-size="18"> {{ rankContent.title }} </v-card-title>        
+      </div>            
+    </v-card> 
+    <div class="d-flex justify-center align-center">      
+      <v-progress-circular v-if="rankCountLoad" indeterminate class="mb-2" color="white"/>    
+      <Observer v-if="rankCountLoad" @triggerIntersected="loadMore"/>
+    </div>
   </div>    
-  <v-progress-circular v-else indeterminate color="primary" class="ProgressCircular" />
+  <v-progress-circular 
+  v-else
+    class="ProgressCircular" 
+    indeterminate 
+    color="white" 
+    :value="100"
+  />
 </template>
 
 <script setup lang="ts">
-  import { computed, onMounted } from "vue"
-  import { useRouter } from "vue-router";
+  import { computed, onMounted, ref } from "vue"
   import { useLayout } from "../mixins/layout";
   import { getMarketValuation, getTodayMarket, getRank } from "../store/payload"
   import { useStockStore } from "../store/stock"
+  import Observer from "../components/Observer.vue";
 
 
   const { market, marketValuation, rank, request } = useStockStore()
-  const router = useRouter()
   const { CONTENT_WIDTH } = useLayout()
+  const rankCount = ref<number>(8)
+  const rankCountLoad = ref<boolean>(true)
 
-  onMounted(async () => {
+  onMounted(() => {
     if (market.data) return
 
-    await request(getRank())
-    await request(getTodayMarket())
-    await request(getMarketValuation())  
-
+    request(getRank())
+    request(getTodayMarket())
+    request(getMarketValuation())  
   })
 
   const rankContents = computed(() => {
     const marcap = rank.data?.marcap || []
     return marcap
       .map(entry => ({ code: entry[1], title: entry[2], market: entry[3], close: entry[4], change: entry[5], changeRatio: entry[6] }))
-      .slice(0, 8)
+      .slice(0, rankCount.value)
   })
+
+  
+
+  const loadMore = () => { 
+    if (rankCount.value > 44) {
+      rankCountLoad.value = false
+      return
+    }
+
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+        rankCount.value += 5 
+        resolve(rankCount.value)
+      }, 500)      
+    })
+  }
 
 
 </script>
