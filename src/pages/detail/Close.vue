564674c84d3253cc83d406fe5ab515ef140a99a0
codma <ruddud1123@naver.com>
2022-12-16 22:31:55 +0900

    가격 트랜지션 추가

--- src/pages/detail/Close.vue
+++ src/pages/detail/Close.vue
@@ -4,69 +4,94 @@
     <StockTitle />
 
     <v-divider />
-    
-
+  
       <!-- <v-card-item :title="stockData.date" /> -->
 
-    <v-card-subtitle class="mt-3"> 
-      종가 Close
-    </v-card-subtitle>
-    <v-card-text class="text-center">
-      <span v-font-size="45">
-        {{ priceFormatter.format(stockData.close) }}
-      </span>        
-      <span v-font-size="17" class="ml-2">
-        {{ computedChanges }}
-      </span>
-      <span>
-        ({{ computedChangeRatio }})
-      </span>
-    </v-card-text>
+    <v-card elevation="0" @click="expandToggle = !expandToggle">
+      <div class="date"> 기준일 : {{ stockData.date }}</div>
+      <v-card-subtitle class="mt-2"> 종가</v-card-subtitle>
+      <v-card-text class="mt-5">
+        <span v-font-size="50">
+          <number
+            :from="0"
+            :to="stockData.close"
+            :format="priceFormat"
+            :duration="1"
+            :delay="0"
+            easing="Power4.easeOut"
+          />        
+        </span>   
+        <transition name="appear">
+          <span v-if="appear">
+            <span v-font-size="17" class="ml-2">
+              {{ computedChanges }}
+            </span>
+            <span>
+              ({{ computedChangeRatio }})
+            </span>
+          </span>
+        </transition>
+      </v-card-text>
+      <v-expand-transition v-if="expandToggle">          
+        <div class="d-flex flex-wrap">
+          <div class="price">
+            <v-card-subtitle> 고가 </v-card-subtitle>    
+            <span v-font-size="28" class="ml-5">
+              <number
+                :from="0"
+                :to="stockData.high"
+                :format="priceFormat"
+                :duration="2"
+                :delay="0"
+                easing="Power4.easeOut"
+              />   
+            </span>        
+          </div>
+          <div class="price">
+            <v-card-subtitle> 저가 </v-card-subtitle>    
+              <span v-font-size="28" class="ml-5">
+                <number
+                  :from="0"
+                  :to="stockData.low"
+                  :format="priceFormat"
+                  :duration="2"
+                  :delay="0"
+                  easing="Power4.easeOut"
+                />   
+              </span>        
+          </div>
+          <div class="price">
+            <v-card-subtitle class="mt-3"> 시가 </v-card-subtitle>    
+              <span v-font-size="28" class="ml-5">
+                <number
+                  :from="0"
+                  :to="stockData.open"
+                  :format="priceFormat"
+                  :duration="2.2"
+                  :delay="0"
+                  easing="Power4.easeOut"
+                />   
+              </span>        
+          </div>
+          <div class="price">
+            <v-card-subtitle class="mt-3"> 거래량 </v-card-subtitle>    
+            <span v-font-size="28" class="ml-5">
+              <number
+                :from="0"
+                :to="stockData.volume"
+                :format="volumeFormat"
+                :duration="2.3"
+                :delay="0"
+                easing="Power4.easeOut"
+              />   
+            </span>        
+          </div>
+        </div>      
+      </v-expand-transition>
+    </v-card>
 
-    <v-divider />
 
-    <div class="d-flex flex-wrap ">
-      <div>
-        <v-card-subtitle class="mt-3"> 
-          고가
-        </v-card-subtitle>    
-        <v-card-text class="text-center">
-          <span v-font-size="35">
-            {{ priceFormatter.format(stockData.high) }}
-          </span>        
-        </v-card-text>
-      </div>
-      <div>
-        <v-card-subtitle class="mt-3"> 저가 </v-card-subtitle>    
-        <v-card-text class="text-center">
-          <span v-font-size="35">
-            {{ priceFormatter.format(stockData.low) }}
-          </span>        
-        </v-card-text>
-      </div>
-      <div>
-        <v-card-subtitle class="mt-3"> 시가 </v-card-subtitle>    
-        <v-card-text class="text-center">
-          <span v-font-size="35">
-            {{ priceFormatter.format(stockData.open) }}
-          </span>        
-        </v-card-text>
-      </div>
-      <div>
-        <v-card-subtitle class="mt-3"> 거래량 </v-card-subtitle>    
-        <v-card-text class="text-center">
-          <span v-font-size="35">
-            {{ stockData.volume.toLocaleString() }}
-          </span>        
-        </v-card-text>
-      </div>
-    </div>
-
-    <v-divider />
-
-
-
-    <v-divider />
+    <v-divider  class="mt-3"/>
 
     <StockCloseChart v-if="loading" :chartData="chartData" />
   </div>
@@ -74,26 +99,41 @@
 
 <script setup lang="ts">
 
-  import { computed } from 'vue'  
+  import { computed, ref, onMounted } from 'vue'  
   import StockCloseChart from '../../components/detail/close/StockCloseChart.vue';
   import { useStockStore } from '../../store/stock';
-  import { priceFormatter } from '../../mixins/tools';
   import StockTitle from '../../components/detail/StockTitle.vue'
+
   const { stockGraphAll, stock } = useStockStore()
+
+  const appear = ref<boolean>(false)
+  const expandToggle = ref<boolean>(false)
 
   const chartData = computed(() => stockGraphAll.data)
   const loading = computed<boolean>(() => !stock.loading && !stockGraphAll.loading)
-
   const stockData = computed(() => stock.data)
+  
   const computedChanges = computed<string>(() => {
     const change = stock.data.changes
-    return change > 0 ? '+₩' + change.toLocaleString() : '₩' + change.toLocaleString()
+    return change > 0 ? '+' + change.toLocaleString() : '₩' + change.toLocaleString()
   })
 
   const computedChangeRatio = computed<string>(() => {
     const changes_ratio = stock.data.changes_ratio as number
     return changes_ratio > 0 ? '+' + changes_ratio.toLocaleString() : '' + changes_ratio.toLocaleString()
   })
+
+  const priceFormat = (price: number) => '₩' + Number(price.toFixed(0)).toLocaleString()
+  const volumeFormat = (price: number) => Number(price.toFixed(0)).toLocaleString()
+
+  onMounted(() => {
+    appear.value = false
+    setTimeout(() => {      
+      appear.value = true
+    }, 1000)
+  })
+
+  
   
 </script>
 
@@ -103,6 +143,32 @@ $margin: 1rem;
   margin: $margin;
   margin-top: 55px;  
 }
+
+.price {
+  width: 180px;
+}
+
+.appear-enter-from {
+  opacity: 0
+}
+
+.appear-enter-to {
+  opacity: 1
+}
+
+.appear-enter-active {
+  transition: all 1s ease-out;
+}
+
+.date {
+  position: absolute;
+  right: 0px;
+  font-size: 12px;
+  opacity: .5;
+  margin-top: 10px;
+}
+
+
 
 
 </style>
