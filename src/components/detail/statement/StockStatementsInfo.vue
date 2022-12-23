9424c9c840b51f05d935715eb6acba47b4cbca86
codma <ruddud1123@naver.com>
2022-12-23 19:01:05 +0900

    statementContents 비지배 제거, 타입명 파스칼케이스로 변경

--- src/components/detail/statement/StockStatementsInfo.vue
+++ src/components/detail/statement/StockStatementsInfo.vue
@@ -1,22 +1,28 @@
 <template>
 
   <v-divider />
-  <v-card 
-    class="CardLayout" 
-    color="cardlayout"
-    v-if="loading"
-  >
-    <div v-for="statementContent in statementContents" :key="statementContent.type">
-      {{ statementContent }}
-    </div> 
-  </v-card>
 
+  <div v-if="loading">
+    <v-sheet
+      class="CardLayout" 
+      color="cardlayout"
+      elevation="0"
+      v-for="statementContent in statementContents" :key="statementContent.type"
+    >
+      <v-card-title>
+        {{ statementContent.type }}
+      </v-card-title>
+      <v-card-text>
+        {{ statementContent.chartData }}
+      </v-card-text>
+    </v-sheet>
+  </div>
 </template>
 
 <script setup lang="ts">
-  import { StockStatementElement } from '@/api/types';
   import { useStockStore } from '@/store/stock';
   import { computed, onMounted } from 'vue'
+  import _ from 'lodash'
 
   type StatementTypes = 'type' |
   'asset' |
@@ -32,31 +38,34 @@
   'revenue'
 
   type StatementContents = {
-    type: StatementTypes,
+    type: StatementTypes | string,
     chartData: number[]
   }
 
+  const banish = ['equity_non', 'profit_non']
+
   const { statement, stock } = useStockStore()
   const loading = computed(() => !statement.loading && !stock.loading)
-  const statementData = computed(() => statement.data)
 
-  const label = computed(() => statement.data.date)
-
-  const statementContents = computed(() => {
+  const statementContents = computed<StatementContents[]>(() => {
     const keys = Object.keys(statement.data.data[0]).slice(1) as StatementTypes[]
-    return keys.map((key: StatementTypes): StatementContents => ({
-      type: key,
+    const removedKeys = keys.filter(key => !banish.includes(key))
+    
+    return removedKeys.map((key: StatementTypes): StatementContents => ({
+      type: toPascalCase(key),
       chartData: statement.data.data.map(v => v[key] as number)
     }))
+
   })
 
-  onMounted(() => {
-    
-  })
-
+  const toPascalCase = (type: string): string => type
+    .replace(/(^\w|_\w)/g, text => text.replace(/_/, '').toUpperCase())
 
 </script>
 
 <style scoped>
+.CardLayout {
+  border-radius: 5px;
+}
 
 </style>
