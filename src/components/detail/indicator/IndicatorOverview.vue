9ee7615d8c256f6449710a0ec1e2e3e5bd054dc1
codma <ruddud1123@naver.com>
2023-01-03 23:08:38 +0900

    IndicatorOverviewChart 컴포넌트 생성

---
+++ src/components/detail/indicator/IndicatorOverview.vue
@@ -0,0 +1,11 @@
+<template>
+  <IndicatorOverviewChart />
+</template>
+
+<script setup lang="ts">
+  import IndicatorOverviewChart from './IndicatorOverviewChart.vue';
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/detail/indicator/IndicatorOverviewChart.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+  
+  import { onMounted, ref } from 'vue';
+  import { Chart } from 'chart.js' 
+
+
+  const chart = ref<Chart | null>()
+
+  const options = {} as any
+
+  const renderChart = () => {
+    const ctx = document.getElementById('IndicatorOverviewChart') as HTMLCanvasElement
+
+    chart.value = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: [],
+        datasets: [{
+          label: '',
+          data: []
+        }]
+      },
+      options
+    })
+  }
+
+  onMounted(() => renderChart())
+
+</script>
+
+
+
+<template>
+  <canvas id="IndicatorOverviewChart"></canvas>
+</template>
