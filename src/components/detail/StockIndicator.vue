0406debee831763dbb1dfc861dbab5dd84fed717
codma <ruddud1123@naver.com>
2023-02-13 02:46:49 +0900

    [삭제] StockIndicator 차트 삭제

--- src/components/detail/StockIndicator.vue
+++ src/components/detail/StockIndicator.vue
@@ -13,14 +13,10 @@
           </v-icon>
           보조지표
         </v-chip>
-      </v-card-title>    
-
-      <v-card-text class="d-flex align-cetner justify-center mt-1">
-        <StockIndicatorChart />      
-      </v-card-text>
-      <v-card-actions class="innerAction">
-        <div class="innerMore-absolute"> 보조지표 확인하기</div>
-      </v-card-actions>
+        <div class="innerMore">
+          재무제표 자세히보기
+        </div>
+      </v-card-title>        
     </v-card>
   </transition>
 </template>
@@ -28,29 +24,23 @@
 <script setup lang="ts">
   import { computed } from 'vue'
   import { useStockStore } from '@/store/stock'
-  import StockIndicatorChart from '@/components/detail/StockIndicatorChart.vue'
   import { useCustomRouter } from '@/mixins/customRouter';
   import { useRoute } from 'vue-router';
 
   const { push } = useCustomRouter()
   const route = useRoute()
   
-
   const { indicator, indicatorSector, indicatorDaily, indicatorSectorDaily } = useStockStore()
   const loading = computed<boolean>(() => !indicator.loading && !indicatorSector.loading && !indicatorSectorDaily.loading && !indicatorDaily.loading)
   
 </script>
 
 <style scoped lang="scss">
-.IndicatorCardLayout {
-  height: 280px;  
-}
 
 .innerAction {
   display: flex;
   justify-content: center;
 }
-
 
 
 .fade-enter-active {
